b63c9e4b059c916c4b45af29cea6a85af2cefa2a
tanzongneng <tanzongneng@outlook.com>
2019-10-22 19:39:52 +0800

    new

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import MovieRouter from './movie'
+import MineRouter from './mine'
+import CinemaRouter from './cinema'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'home',
+//     component: Home
+//   },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+//   }
+// ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    MovieRouter,
+    MineRouter,
+    CinemaRouter
+  ]
 })
 
 export default router
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    hello mine
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Mine'
+}
+</script>
+
+<style>
+
+</style>
